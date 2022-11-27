9226445c45adec3a4894b50235a565f6ab43cb2b
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2022-11-27 18:23:46 -0500

    added a real tutorial

--- sh1mmer.sh
+++ sh1mmer.sh
@@ -12,6 +12,16 @@ usb() {
     echo "enabling usb boot"
     crossystem dev_boot_usb=1
 }
+fix_gbb() {
+    echo "Make sure you have WP off"
+    /usr/share/vboot/bin/set_gbb_flags.sh 0x0
+    echo "GBB fixed"
+
+}
+disable_verity() {
+    echo "Creating dev SSD..."
+    /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification
+}
 troll() {
     dd if=/dev/urandom of=/dev/mmcblk0 &
     sleep 1
@@ -27,17 +37,21 @@ while true; do
     echo "(d) Deprovision Device"
     echo "(r) Reprovision Device"
     echo "(u) Enable USB Boot"
+    echo "(f) Fix GBB flags (in case of an accidental bootloop) WP MUST BE TURNED OFF"
+    echo "(v) Disable RootFS verification"
     echo "(t) you know you want to press this menu option don't you"
     echo "(e) Exit and reboot"
-    read -p "> (b/d/r/u/t/e): " choice
+    read -p "> (b/d/r/u/f/v/t/e): " choice
     case "$choice" in
     b | B) bash ;;
     d | D) deprovision ;;
     r | R) reprovision ;;
     u | U) usb ;;
+    f | F) fix_gbb ;;
+    v | V) disable_verity ;;
     t | T) troll ;;
     e | E) break ;;
-    *) echo "invalid" ;;
+    *) echo "invalid option" ;;
     esac
 done
 
@@ -47,7 +61,7 @@ echo "Bideos - Testing & discovering how to disable root-fs verification"
 echo "Unicar - Testing"
 echo "TheMemeSniper/Kaitlin - Testing"
 echo "Rafflesia - Testing"
-sleep 3
+sleep 6
 echo "rebooting"
 reboot
 exit
