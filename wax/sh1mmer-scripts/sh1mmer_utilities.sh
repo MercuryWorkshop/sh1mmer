053727977900224d4cc258d010ff9dfd83c8a5b3
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-01-08 23:20:38 -0500

    wax fixed, added devmode unblocker to utilites, added feedback for utilities

--- wax/sh1mmer-scripts/sh1mmer_utilities.sh
+++ wax/sh1mmer-scripts/sh1mmer_utilities.sh
@@ -21,7 +21,19 @@ fix_gbb() {
 disable_verity() {
     /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk0 --remove_rootfs_verification
 }
+unblock_devmode() {
+    vpd -i RW_VPD -s check_enrollment=0
+    vpd -i RW_VPD -s block_devmode=0
+    crossystem block_devmode=0
 
+    # the sleeps here are so cryptohome won't get pissy
+
+    tpm_manager_client take_ownership
+    sleep 2
+    cryptohome --action=tpm_take_ownership
+    sleep 2
+    cryptohome --action=remove_firmware_management_parameters
+}
 shell() {
     cleanup
     bash
@@ -29,46 +41,60 @@ shell() {
     clear
     sleep 0.1
 }
+runtask() {
+
+    # instead of clearing maybe we could use one of the term uis? idk how to make it properly aligned though, so a TODO for r58
+    cleanup
+    echo "Starting task $1"
+    sleep 2
+    if $1; then
+        echo "Task $1 succeeded."
+        sleep 3
+    else
+        read -p "THERE WAS AN ERROR! The utility likely did not work. Press any key to continue."
+    fi
+    clear
+}
 
 credits() {
     showbg Credits.png
     movecursor_Credits 0
-    echo -n "CoolElectronics: Creating the original script"
+    echo -n "CoolElectronics: Finding the original vulnerability"
     movecursor_Credits 1
-    echo -n "r58Playz: Creating the GUI script"
+    echo -n "r58Playz: Creating the Beautiful World GUI"
     movecursor_Credits 2
-    echo -n "rabbithawk256: Providing GUI assets"
+    echo -n "rabbithawk256: Designing GUI assets"
     movecursor_Credits 3
-    echo -n "ULTRA BLUE: Testing & discovering how to disable rootfs verification"
+    echo -n "ULTRA BLUE: Discovering how to unlock the rootfs"
     movecursor_Credits 4
-    echo -n "Sharp_Jack: Creating the wax automation tool"
+    echo -n "Sharp_Jack: Creating the inital version of wax automation tool"
     movecursor_Credits 5
     echo -n "Unciaur: Finding the first shim"
     movecursor_Credits 6
     echo -n "TheMemeSniper: Testing"
     movecursor_Credits 7
-    echo -n "OlyB: Scraping more shims"
+    echo -n "OlyB: Scraping additional shims"
     movecursor_Credits 8
     echo -n "Rafflesia: Hosting"
     printf "\033[0;0H"
     while true; do
-        case `readinput` in
-            'kB') break ;;
+        case $(readinput) in
+        'kB') break ;;
         esac
     done
 }
 
 while true; do
     showbg Utilities.png
-    case `readinput` in
-	'kB') break ;;
-	'1') fix_gbb 2>/dev/null ;;
-	'2') deprovision 2>/dev/null ;;
-	'3') reprovision 2>/dev/null ;;
-	'4') usb 2>/dev/null;;
-	'5') disable_verity 2>/dev/null;;
-	'6') shell ;;
-	'7') credits;;
+    case $(readinput) in
+    'kB') break ;;
+    '1') runtask fix_gbb ;;
+    '2') runtask deprovision ;;
+    '3') runtask reprovision ;;
+    '4') runtask usb ;;
+    '5') runtask disable_verity ;;
+    '6') runtask unblock_devmode ;;
+    '7') shell ;;
+    '8') credits ;;
     esac
 done
-
--- wax/wax.sh
+++ wax/wax.sh
@@ -5,36 +5,63 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 echo "-------------------------------------------------------------------------------------------------------------"
-echo "Welcome to wax, a shim modifying automation tool made by CoolElectronics and Sharp_Jack, improved by r58playz"
-echo "Prerequisites: cgpt must be installed, program must be ran as root"
+echo "Welcome to wax, a shim modifying automation tool made by CoolElectronics and Sharp_Jack, greatly improved by r58playz and Rafflesia"
+echo "Prerequisites: cgpt must be installed, program must be ran as root, chromebrew.tar.gz needs to exist"
 echo "-------------------------------------------------------------------------------------------------------------"
-read -p "please enter the path do your .bin file (example: /home/username/Downloads/sentry.bin) : " bin
-lsblk
-read -p "Out of the devices above, enter the path to the usb you want to flash (example: /dev/sdb) : " usb
 
-flash() {
-    echo flashing $1 to $2
-    dd if=$1 of=$2 status=progress
-}
+bin=$1
 
-# clean up if exited abnormally
-umount "${usb}3" || :
-rm -rf mnt || :
+echo "Expanding bin for 'arch' partition"
+dd if=/dev/zero bs=1G count=6 >>$bin
+echo -ne "\a"
+# Fix corrupt gpt
+fdisk $bin <<EOF
+w
 
-echo "Flashing raw shim"
-flash $bin $usb
+EOF
+
+# create new partition filling rest of disk
+fdisk $1 <<EOF
+n
+
+
+
+w
+EOF
+echo "Creating loop device"
+loop=$(losetup -f)
+losetup -P $loop $bin
+
+echo "Making arch partition"
+mkfs.ext4 -L arch ${loop}p13
 echo "Making ROOT mountable"
-sh make_dev_ssd_no_resign.sh --remove_rootfs_verification -i $usb
-sleep 2
+sh make_dev_ssd_no_resign.sh --remove_rootfs_verification -i ${loop}
 echo "Creating Mountpoint"
 mkdir mnt || :
+mkdir mntarch || :
 echo "Mounting ROOT-A"
-mount "${usb}3" mnt
+mount "${loop}p3" mnt
+echo "Mounting arch"
+mount "${loop}p13" mntarch
+
+echo "Accquiring chromebrew"
+# wget "https://files.alicesworld.tech/chromebrew.tar.gz"
+# uncomment this line when file servers go public or add the creds yourself
+echo "Extracting chromebrew"
+cd mntarch
+tar xvf ../chromebrew.tar.gz --strip-components=1
+cd ..
 echo "Injecting payload"
-sleep 5
-cp ../sh1mmer.sh mnt/usr/sbin/factory_install.sh
+cp -rv sh1mmer-assets mnt/usr/share/sh1mmer-assets
+cp -v sh1mmer-scripts/* mnt/usr/sbin/
+cp -v factory_install.sh mnt/usr/sbin/
 echo "Cleaning up..."
-sleep 5
-umount "${usb}3"
-rm -rf mnt
-echo "Done. Have fun"
+sync
+if umount "${loop}p3" && umount "${loop}p13"; then
+    rm -rf mnt # If we don't check whether the unmount succeeds, these lines wipe the rootfs.
+    rm -rf mntarch
+    losetup -d ${loop}
+else
+    echo "Couldn't safely unmount. Please unmount and detach the loopbacks yourself."
+fi
+echo "Done. Have fun!"
