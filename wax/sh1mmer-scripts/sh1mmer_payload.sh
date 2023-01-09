13ad8b7b3da8c190ad6f67203319b1eddbb32772
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-01-09 16:04:08 -0500

    bootstrap new menu

--- wax/sh1mmer-scripts/sh1mmer_payload.sh
+++ wax/sh1mmer-scripts/sh1mmer_payload.sh
@@ -4,6 +4,14 @@ shopt -s nullglob
 
 showbg terminalGeneric.png
 
+mapname() {
+	case $1 in
+	'/usr/local/payloads/wifi.sh') return 'Connect to wifi' ;;
+	'/usr/local/payloads/autoupdate.sh') return 'Fetch updated payloads. REQUIRES WIFI' ;;
+	'/usr/local/payloads/troll.sh') return "hahah wouldn't it be realllly funny if you ran this payload" ;;
+	esac
+}
+
 selectorLoop() {
 	selected=0
 	while true; do
@@ -11,18 +19,24 @@ selectorLoop() {
 		for opt; do
 			movecursor_generic $idx
 			if [ $idx -eq $selected ]; then
-				echo -n "--> $opt"
+				echo -n "--> $(mapname $opt)"
 			else
 				echo -n "    $opt"
 			fi
 			((idx++))
 		done
-		input=`readinput`
+		input=$(readinput)
 		case $input in
-			'kB') exit ;;
-			'kE') return $selected ;;
-			'kU') ((selected--)); if [ $selected -lt 0 ]; then selected=0; fi ;;
-			'kD') ((selected++)); if [ $selected -ge $# ]; then selected=$(($#-1)); fi ;;
+		'kB') exit ;;
+		'kE') return $selected ;;
+		'kU')
+			((selected--))
+			if [ $selected -lt 0 ]; then selected=0; fi
+			;;
+		'kD')
+			((selected++))
+			if [ $selected -ge $# ]; then selected=$(($# - 1)); fi
+			;;
 		esac
 	done
 }
