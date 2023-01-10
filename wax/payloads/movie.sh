b7ce9a6a3f2bed997bb2c29ac53ceba3421f3a1c
r58Playz <r58playz@gmail.com>
2023-01-09 18:13:06 -0800

    hotfix movie payload

--- wax/payloads/movie.sh
+++ wax/payloads/movie.sh
@@ -1,7 +1,7 @@
 source /usr/local/payloads/gui_lib.sh
 showimage /usr/share/sh1mmer-assets/Logs.png
 for file in /usr/local/payloads/movie-supporting/*.png; do
-	showimageScale $file 2
+	showimage $file
 	sleep 0.03
 done
 
