714cd14a5e278f32fdf5acf7b1ff5a49bd6b847a
DerDer56 <63243328+DerDer56@users.noreply.github.com>
2023-01-12 19:53:54 -0500

    Update index.js
    idk

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ pc:"Or if you can download the recovery utility extension on your school compute
 board:"It's somewhere in the firmware log/debug info on the recovery screen. It is also shown at the bottom of the Android Preferences app. You can also go to a connected WiFi network in settings, click the Learn More link next to the message about syncing, and your board name will be somewhere in the URL of the newly-opened help tab.",
 launch:"Click the extensions icon in Chrome, then click Chromebook Recovery Utility."
 }
-
-document.querySelectorAll('u').forEach(elem=>{
-  elem.onclick=function(){
-    alert(strings[this.message])
-  }
-})
