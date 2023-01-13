5691231e71ed585162801c93e745a0e8bc29072b
DerDer56 <63243328+DerDer56@users.noreply.github.com>
2023-01-12 19:48:15 -0500

    Update index.js
    functionality to underlined text snippets

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+let strings = {
+mercury:"Check the credits button at the bottom of the page for more info.",
+monday:"We wanted it to be Friday the 13th for the funnies but it didn't end up happening...",
+pc:"Or if you can download the recovery utility extension on your school computer, but that's exceedingly rare.",
+board:"It's somewhere in the firmware log/debug info on the recovery screen. It is also shown at the bottom of the Android Preferences app. You can also go to a connected WiFi network in settings, click the Learn More link next to the message about syncing, and your board name will be somewhere in the URL of the newly-opened help tab.",
+launch:"Click the extensions icon in Chrome, then click Chromebook Recovery Utility."
+}
+
 document.querySelectorAll('u').forEach(elem=>{
   elem.onclick=function(){
-    alert(this.message)
+    alert(strings[this.message])
   }
 })
