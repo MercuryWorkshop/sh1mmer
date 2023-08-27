4f4521ef458e66929aba068334fc9082c78111d6
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2023-08-27 16:12:05 +0000

    Create wax-mini.sh
    This script was extracted from bzImage, modified to work in a standard linux environment.
    
    also mercury workshop try not to make a typo challenge ("CROMEBREW" to "CHROMEBREW")
    
    also mercury workshop try not to blindly copy paste code challenge ("mrchromebox.sh" was included in the mini shim, but "mrchromebox-supporting" was not)

---
+++ wax/payloads-mini/mrchromebox-supporting/README.md
@@ -0,0 +1,2 @@
+MrChromebox's ChromeOS Firmware Utility script modified for a shim environment
+None of these files are owned by Mercury Workshop
---
+++ wax/payloads-mini/mrchromebox.sh
@@ -0,0 +1,5 @@
+source /usr/local/payloads/gui_lib.sh
+allowtext
+clear
+bash /usr/local/payloads/mrchromebox-supporting/firmware-util.sh
+disallowtext
