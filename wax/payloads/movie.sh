b15795e77385c74aee417873d50981052e5b14b4
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2023-11-18 17:59:01 -0800

    return of microsoft movie!!!
    shittified with `pngquant movie_payload/* --ext .png -f --speed 11 --quality 60-70`
    then .tar.gz

--- wax/payloads/movie.sh
+++ wax/payloads/movie.sh
@@ -1,12 +1,12 @@
 source /usr/local/payloads/lib/gui_lib.sh
 showimage /usr/share/sh1mmer-assets/Logs.png
 
-if [ -f /usr/local/payloads/movie_payload.tar ]; then
-	echo "extracting movie_payload.tar"
+if [ -f /usr/local/payloads/movie_payload_pngquant.tar.gz ]; then
+	echo "extracting movie_payload_pngquant.tar.gz"
 	mkdir /tmp/movie_payload
-	tar -xf /usr/local/payloads/movie_payload.tar -C /tmp/movie_payload
+	tar -xf /usr/local/payloads/movie_payload_pngquant.tar.gz -C /tmp/movie_payload
 else
-	echo "movie_payload.tar not found!" >&2
+	echo "movie_payload_pngquant.tar.gz not found!" >&2
 	exit 1
 fi
 
